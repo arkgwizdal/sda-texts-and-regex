a28508646917c4be62a14d80c116a5d9126e1d3e
Arkadiusz Gwizdal <ark.gwizdal@gmail.com>
2022-12-13 20:00:12 +0100

    Dodanie rozwiązania Regex004

---
+++ src/regex/Regex004.java
@@ -0,0 +1,22 @@
+/*
+Napisz Regex,
+sprawdza czy użytkownik wprowadził poprawną nazwę miejscowosci.
+
+Na przykład Wroclaw, Zielona Gora czy Bielsko-Biala jest ok, jednak Ptysiow123 już nie.
+Dla uproszczenia załóżmy, że żadna nazwa miejscowości nie zawiera polskich znaków.
+ */
+package regex;
+
+public class Regex004 {
+    public static void main(String[] args) {
+        RegexTester tester = new RegexTester("[A-ZŻŹĆŃÓŁĘĄŚ][a-zżźćńółęąś]+([ \\-][A-Z][a-zżźćńółęąś]+)?");
+        tester.validate("Łódź");
+        tester.validate("Wroclaw");
+        tester.validate("Wrocław");
+        tester.validate("Zielona Gora");
+        tester.validate("Bielsko-Biala");
+        tester.validate("Bielsko-Biała");
+        tester.validate("Ptysiow123");
+        tester.validate("warszawa");
+    }
+}
